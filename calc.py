e909dc2fd554e7ccf8a57645e0576323524f4a44
sadpatil <54612563+sadpatil@users.noreply.github.com>
2019-08-29 09:21:23 +0530

    end

--- calc.py
+++ calc.py
@@ -3,11 +3,11 @@
 def subtract(x,y):
     PASS
 def multiply(x,y):
-    pass
+    PAA
 def divide(x,y):
     PASS
 def square(x,y):(x,y)
-    pass
+    PASS
 def multiply(x,y):
     return x*y
 final commit
